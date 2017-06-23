9fe8628eb6d09c0672c5835411001419642cc3ec
fazt <fabianncdev@gmail.com>
2017-06-23 16:13:08 -0500

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom';
+
+const App = () => <h1>Hola Mund 2</h1>
+
+render(
+  <App/>,
+  document.getElementById('app')
+);
---
+++ readme.md
@@ -0,0 +1,2 @@
+## Considerations
+- I'm Ignored the bundle.js file on the dist folder
